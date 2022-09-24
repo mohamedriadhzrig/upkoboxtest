aef87596fb508d528188712bd50552112d4ee2c2
benking110181 <benking110181@gmail.com>
2022-09-25 00:45:28 +0200

    sh

--- leia/plugin.program.Skodi-0.0.1/resources/Backup Check.py
+++ leia/plugin.program.Skodi-0.0.1/resources/Backup Check.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #kick off the Kodi backup
 Kodi.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Addons.ExecuteAddon","params":{"addonid":"script.xbmcbackup","params":{"mode":"backup"}}, "id": 1 }')
 
@@ -11,18 +10,4 @@ while (window.getProperty('script.xbmcbackup.running') == 'true'):
      #do something here, probably just sleep for a few seconds
      Kodi.sleep(5000)
 
-=======
-#kick off the Kodi backup
-Kodi.executeJSONRPC('{ "jsonrpc": "2.0", "method": "Addons.ExecuteAddon","params":{"addonid":"script.xbmcbackup","params":{"mode":"backup"}}, "id": 1 }')
-
-#sleep for a few seconds to give it time to kick off
-Kodi.sleep(10000)
-
-window = Kodigui.Window(10000)
-
-while (window.getProperty('script.xbmcbackup.running') == 'true'):
-     #do something here, probably just sleep for a few seconds
-     Kodi.sleep(5000)
-
->>>>>>> ae015f14fa26bfbc816a3b195d7f8308e35e6511
 #backup is now done, continue with script
--- leia/plugin.program.Skodi-0.0.1/resources/settings fenetre U2P.py
+++ leia/plugin.program.Skodi-0.0.1/resources/settings fenetre U2P.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 xbmc.executebuiltin('Addon.OpenSettings(plugin.video.sendtokodiU2P)') 
-=======
-xbmc.executebuiltin('Addon.OpenSettings(plugin.video.sendtokodiU2P)') 
->>>>>>> ae015f14fa26bfbc816a3b195d7f8308e35e6511
