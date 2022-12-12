3f0dbfec8941c8e54c1d515bcbff0af50b1e157b
benking110181 <benking110181@gmail.com>
2022-12-12 12:02:37 +0100

    service.majhk

--- matrix/service.majhk/service.py
+++ matrix/service.majhk/service.py
@@ -1,18 +1,17 @@
 import time
 import xbmcaddon
-import threading
-import datetime
 import sys
+import xbmcvfs
+import sqlite3
+
 pyVersion = sys.version_info.major
 pyVersionM = sys.version_info.minor
 if pyVersionM == 11:
     import cryptPaste11 as cryptage
     import scraperUPTO11 as scraperUPTO
 elif pyVersionM == 8:
-    #import pasteCrypt3 as cryptage
     import cryptPaste8 as cryptage
     import scraperUPTO8 as scraperUPTO
-    #import scraperUPTO
 elif pyVersionM == 9:
     import cryptPaste9 as cryptage
     import scraperUPTO9 as scraperUPTO
@@ -20,10 +19,11 @@ elif pyVersionM == 10:
     import cryptPaste10 as cryptage
     import scraperUPTO10 as scraperUPTO
 
-time.sleep(1)
+time.sleep(0.1)
 addon = xbmcaddon.Addon("plugin.video.sendtokodiU2P")
 interval = int(addon.getSetting("intmaj"))
 delaiMaj = int(addon.getSetting("delaimaj"))
+#scraperUPTO.extractEpisodesOnContinue()
 if interval:
     time.sleep(delaiMaj * 60)
     monitor = xbmc.Monitor()
