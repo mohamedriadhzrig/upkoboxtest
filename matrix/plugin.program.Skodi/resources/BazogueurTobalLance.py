aa46daa1c4c0844c8256ea12196fda45829fb525
benking110181 <bva3525@gmail.com>
2023-01-10 19:32:31 +0100

    plugin.program.Skodi

--- matrix/plugin.program.Skodi/resources/BazogueurTobalLance.py
+++ matrix/plugin.program.Skodi/resources/BazogueurTobalLance.py
@@ -1,5 +1,5 @@
 import xbmc
-xbmc.executebuiltin('RunPlugin(plugin://plugin.program.Skodi/?action=BazogueurTobalLance)')
+xbmc.executebuiltin('RunPlugin(plugin://plugin.program.Skodi/?action=BazogueurTobalLanceZephyr)')
 xbmc.sleep(1000)
 xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skin","value":"skin.estuary"}}')
 xbmc.sleep(100)
