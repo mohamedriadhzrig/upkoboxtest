46a60463d98c3f91d87187b9b68cdea4d1999b03
benking110181 <bva3525@gmail.com>
2023-01-10 20:49:43 +0100

    plugin.program.Skodi (0.1.1~beta3)

--- matrix/plugin.program.Skodi/resources/BazogueurTobaLance.py
+++ matrix/plugin.program.Skodi/resources/BazogueurTobaLance.py
@@ -1,5 +1,8 @@
-import xbmc
-xbmc.executebuiltin('RunPlugin(plugin://plugin.program.Skodi/?action=BazogueurTobaLanceZephyr)')
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import xbmc, xbmcgui
+
+xbmc.executebuiltin('RunPlugin(plugin://plugin.program.Skodi/?action=BazogueurTobaLance)')
 xbmc.sleep(1000)
 xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skin","value":"skin.estuary"}}')
 xbmc.sleep(100)
@@ -7,6 +10,9 @@ xbmc.executebuiltin('SendClick(11)')
 xbmc.sleep(100)
 xbmc.executebuiltin('System.Logoff()')
 xbmc.sleep(100)
-xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skin","value":"skin.arctic.zephyr.mod"}}')
-xbmc.sleep(100)
 xbmc.executebuiltin('SendClick(11)')
+xbmc.executebuiltin("ActivateWindow(Programs)")
+xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue","id":1,"params":{"setting":"lookandfeel.skin","value":"skin.arctic.zephyr.mod"}}')
+xbmc.executebuiltin('SendClick(11)')
+dialog = xbmcgui.Dialog()
+dialog.ok("Skin Changer", "SkIN DOIT AVOIR CHANGÉ")
